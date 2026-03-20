09d9d50369e6cbec7e0f14ce660f3d32fc743d9d
Pritam3692 <pp3692@srmist.edu.in>
2026-03-20 15:07:02 +0530

    Preetam UC2

---
+++ Users/Admin/Documents/Palindrome_CheckerApp/src/UC1.java
@@ -0,0 +1,21 @@
+
+
+public class UC1 {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("===================================");
+        System.out.println("   Welcome to Palindrome Checker App");
+        System.out.println("===================================");
+
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+
+
+        System.out.println("\nApplication started successfully!");
+        System.out.println("Ready for palindrome checking...\n");
+    }
+}

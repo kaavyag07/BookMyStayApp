36e7734ebf52325f7a55ad8db748ed0fcfc465a8
kaavyag07 <kaavyagupta2007@gmail.com>
2026-04-01 22:23:52 +0530

    Add booking validation exception

---
+++ src/InvalidBookingException.java
@@ -0,0 +1,8 @@
+/**
+ * Custom exception for invalid booking scenarios.
+ */
+public class InvalidBookingException extends Exception {
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}

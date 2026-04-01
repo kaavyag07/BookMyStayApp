0bc30ab7a4d2838f1a85eeecbe3e098fe776a16c
kaavyag07 <kaavyagupta2007@gmail.com>
2026-04-01 22:23:50 +0530

    Add room implementations

---
+++ src/DoubleRoom.java
@@ -0,0 +1,11 @@
+/**
+ * Concrete room type representing a double room.
+ *
+ * @author kaavyag07
+ * @version 2.0
+ */
+public class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("DoubleRoom", 2, 400, 2500.0);
+    }
+}
